ab1ca15437a00fa64101ced69a69c9a0d4079616
riyanwardhana <riyanwardhana2@gmail.com>
2026-04-01 23:57:56 +0700

    Add: Install ReactJS

---
+++ sigizi-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-sigizi-green mb-4">
+          SI-GIZI SIGAP
+        </h1>
+        <p className="text-lg text-gray-600">
+          Sistem Deteksi Dini & Pemetaan Kerentanan Gizi Anak
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
